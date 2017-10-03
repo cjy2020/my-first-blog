550b4668cb98c05387e4b1cae9d56d4daeffad07
jinyu chu <cjy2020@gmail.com>
2017-10-03 18:32:13 +0800

    完善web
    完成表单制作

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,17 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
 
-    
+
     class Meta:
         model=Post
-        fileds=('title','text')
+        fields=('title','text',)
+
+# from django import forms
+#
+# from .models import Post
+#
+# class PostForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'text',)
